7e5e5f9c08add699eb7c38de9b4db4739b4952ea
Olive Maunupau <olivemaunupau@Olives-MacBook-Pro.local>
2024-07-09 11:02:27 -0700

    sidebar fixed

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -8,7 +8,7 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-//import { LightMode } from "@mui/icons-material";
+import { LightMode } from "@mui/icons-material";
 
 const Topbar = () => {
     const theme = useTheme();
